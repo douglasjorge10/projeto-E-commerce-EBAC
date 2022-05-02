367b4866983546c51d8619fd13dd3c33cc1b59e5
Douglas J. Carvalho <dev-douglas@outlook.com.br>
2022-05-02 19:47:06 -0300

    add bibliotecas JQuery, modal contato

--- js/custom.js
+++ js/custom.js
@@ -1,5 +1,3 @@
-// instancia jquery e evita conflitos
-// jQuery( function($){
 $(document).ready(function(){
 
     $('#owl-carousel').owlCarousel();
@@ -37,7 +35,7 @@ $(document).ready(function(){
             nav:true
         },
         600:{
-            items:2,
+            items:3,
             nav:false
         },
         1000:{
@@ -46,38 +44,93 @@ $(document).ready(function(){
             loop:false
         }
     }
-    //$('.featured-item:first h4').append('<span class="badge bg-secondary">Novo</span>')
-    // $('.featured-item:first h4').start('<span class="badge bg-secondary">Novo</span>')
-    // $('.featured-item:first h4').html('<span class="badge bg-secondary">Novo</span>')
-    // $('.featured-item:first h4').addClass('active')
-    // $('.featured-item:first h4').removeClass('active')
-    // $('.featured-item:first h4').toggleClass('active')
-    // $('.featured-item:first h4').hide()
-    // $('.featured-item:first h4').show()
-    // $('.featured-item:first h4').fadeIn(2000)
-    // $('.featured-item:first h4').fadeOut()
-    //  $('.featured-item:first h4').css('color', '#f00')
-     
+});
+/* modais sobre e contato */
+$('.nav-modal-open').on('click', function(e){
 
-     /*
-      * Manipulação de eventos
-      
-     $('.featured-item a').on('blur', function(event){
+    e.preventDefault();
 
-        event.preventDefault();
+    let elem = $(this).attr('rel')
 
-        alert('Produto esgotado');
+    $('.modal-body').html($('#'+elem).html())
+    
+    $('.modal-header h5.modal-title').html($(this).text())
 
-     })*/
-     
-     
-     
-     
+    let myModal = new bootstrap.Modal($('#modelId'))
+
+    myModal.show()
+
+ });
+
+ function validate( elem ){
+    if( elem.val() == '') {
+
+       console.log('o campo de '+ elem.attr('name') + ' é obrigatório')
+
+       elem.parent().find('.text-muted').show()
+
+       elem.addClass('invalid')
+
+       return false
+    } else {
+       elem.parent().find('.text-muted').hide()
+       elem.removeClass('invalid')
+    }
+ }
+
+ $('body').on('submit', '.modal-body .form', function(e){
+
+    e.preventDefault()
+
+    const inputName = $('#nome')
+    const inputEmail = $('#email')
+
+    validate(inputName)
+    validate(inputEmail)
+
+    if(inputEmail.hasClass('invalid') || inputName.hasClass('invalid')){
+       console.log('verificar campos obrigatórios')
+       return false
+    } else {
+       $(this).submit()  
+    }
+
+ })
+
+ $('body').on('blur', '#nome', function(){
+    validate($(this))
+ })
+
+ $('body').on('blur', '#email', function(){
+    validate($(this))
+ })
 
 
+ $('body').on('focus', '#date', function(){
+    $(this).datepicker()
+ })
 
+ $('body').on('blur', '#date', function(){
+    validate($(this))
+    $(this).mask('00/00/0000');
+ }) 
 
+ $('body').on('blur', '#time', function(){
+    validate($(this))
+    $(this).mask('00:00');
+ })
 
+ $('body').on('blur', '#cep', function(){
+    validate($(this))
+    $(this).mask('00000-000');
+ })
 
+ $('body').on('blur', '#phone', function(){
+    validate($(this))
+    $(this).mask('00000-0000');
+ })
 
-})
+ $('body').on('blur', '#cpf', function(){
+    validate($(this))
+    $(this).mask('000.000.000-00');
+ })
